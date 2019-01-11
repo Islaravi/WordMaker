e67fd0136819699baf11fc80c2f6150baf1105db
Islaravi <42480811+Islaravi@users.noreply.github.com>
2019-01-11 04:17:43 -0500

    Create secret.js

---
+++ js/secret.js
@@ -0,0 +1,49 @@
+/**
+ * Created by jdifebo on 3/12/2016.
+ */
+"use strict";
+
+var model;
+var modelIsDirty = true;
+
+function generate() {
+    var minLength = parseInt($("#minimumLength").val());
+    var maxLength = parseInt($("#maximumLength").val());
+    var matchingText = $("#mustContain").val();
+    var matchingRegex = new RegExp(matchingText);
+    $(".generated").each(function() {
+        $( this ).text(model.generateWord(minLength, maxLength, matchingRegex));
+    });
+}
+
+function loadPremadeWordList() {
+    var filename = "resources/secret.txt";
+    $.get(filename, function( data ) {
+        $("#WordList").val( data );
+        var numWords = data.split("\n").length;
+        if (numWords > 1000){
+            $("#MarkovChainOrder").val(3);
+        }
+        else {
+            $("#MarkovChainOrder").val(3);
+        }
+    });
+    modelIsDirty = true;
+}
+
+function buildModelAndGenerateWords() {
+    if (modelIsDirty) {
+        modelIsDirty = false;
+        var words = $("#WordList").val().split(/\s+/);  // whitespace divides the words
+        model = buildMarkovModel(words, parseInt($("#MarkovChainOrder").val()));
+    }
+    generate();
+}
+
+$(document).ready(function() {
+    $("#generate").click(buildModelAndGenerateWords);
+    loadPremadeWordList();
+    $("#premadeWordList").change(loadPremadeWordList);
+    $("#WordList").change(function(){modelIsDirty = true});
+    $("#MarkovChainOrder").change(function(){modelIsDirty = true});
+});
